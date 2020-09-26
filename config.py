2691489bce1431e9e5fa23abeee6dc676892399b
John Doe <johndoe@example.com>
2020-09-26 00:37:01 +0000

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import telebot
+from telebot import types
+
+admin = 416744346
+admin2 = 1026492914
+bot_name = "playboygames_bot"
+
+# bot_token='1365172510:AAFTEgRS1ridHfgXu3_gF5GgNRDgRCZAtBg'
+bot_token='1309980513:AAFmL3gmAM1dwXUfHTiK__76H6Q3EtlHMsM'
+
+subid = -416744346
